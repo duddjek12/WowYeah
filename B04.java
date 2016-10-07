d4518d7f8ae314cb373f79892cabd9bd0694f739
duddjek12 <abra132@naver.com>
2016-10-07 18:24:46 +0900

    Update B04.java

--- B04.java
+++ B04.java
@@ -1,2 +1,18 @@
-sdasdasdasd
-
+import java.util.*;
+public class Money{
+  
+  public static void main(String[] args) {
+  new B04(),money(0;
+  }
+    void money(){
+    double money;
+    double a;
+    Scanner m = New scanner(System.in);
+    System.out.println("연봉을 입력하세요.");
+    money = sc.nextdouble();
+    
+    if(money <=10000000)
+      a=money*0.095;
+    else if(money > 10000000 && money <= 40000000)
+      a=
+  }
